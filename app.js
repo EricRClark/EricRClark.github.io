f9827c7ff90a2bf18dfc5efba74b5661d795268b
EricRClark <eric.erandy@gmail.com>
2016-05-09 22:55:49 -0400

    updated layout

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
-    page.init();
+    // page.init();
+
+
+$(".flip").hover(function(){
+  $(this).find(".card").toggleClass("flipped");
+  return false;
 });
-
-var page = {
-    init: function() {
-        page.styling();
-        page.events();
-          },
-
-    styling: function() {
-            page.projectsGet();
-            page.contactGet();
-            page.aboutGet();
-          },
-
-    events: function() {
-      $('.button.-projects').on('click', page.projectsGet);
-      $('.button.-contact').on('click', page.contactGet);
-      $('.button.-about').on('click', page.aboutGet);
-          },
-
-    projectsGet: function () {
-      var tmpl = _.template(template.projectsTemplate)
-    }
-}
+});
+// var page = {
+//     init: function() {
+//         page.styling();
+//         page.events();
+//           },
+//
+//     styling: function() {
+//             page.projectsGet();
+//             page.contactGet();
+//             page.aboutGet();
+//           },
+//
+//     events: function() {
+//       $('.button.-projects').on('click', page.projectsGet);
+//       $('.button.-contact').on('click', page.contactGet);
+//       $('.button.-about').on('click', page.aboutGet);
+//           },
+//
+//     projectsGet: function () {
+//       var tmpl = _.template(template.projectsTemplate)
+//     }
+// }
